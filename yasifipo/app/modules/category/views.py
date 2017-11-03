b744454a8518062ae1600123c5314cf8a726d2b5
Julien Duroure <julien.duroure@gmail.com>
2017-11-03 12:04:24 +0100

    Put category retrieving stuff into own function, to reuse it for categories

--- yasifipo/app/modules/category/views.py
+++ yasifipo/app/modules/category/views.py
@@ -1,7 +1,6 @@
 from flask import render_template
 
 from .helpers import *
-from frontmatter import load
 
 def display_categories(lang):
 	return 'TODO'
@@ -19,39 +18,7 @@ def display_category(category, lang):
 
 	langs = get_category_langs(category)
 
-	items = {}
-	for type_ in app.yasifipo["cat_ref"][category][lang].keys():
-		for item in app.yasifipo["cat_ref"][category][lang][type_]:
-			if type_ in ["course", "toc"]:
-				with open(app.yasifipo["refs"][item][lang]['file'] + "/.chapter.md") as fil_:
-					yaml = load(fil_)
-			else:
-				with open(app.yasifipo["refs"][item][lang]['file']) as fil_:
-					yaml = load(fil_)
-
-			if type_ not in items.keys():
-				items[type_] = {}
-				items[type_]['items'] = []
-
-			if type_ == "page":
-				items[type_]['items'].append({ 'title': yaml['title'] , 'url': yasifipo_url_for('display_page', file_= app.yasifipo["refs"][item][lang]['file'] )})
-			elif type_ == "prez":
-				items[type_]['items'].append({ 'title': yaml['title'] , 'url': yasifipo_url_for('display_prez', file_= app.yasifipo["refs"][item][lang]['file'], lang=lang, single=False )})
-			elif type_ == "prez-single":
-				items[type_]['items'].append({ 'title': yaml['title'] , 'url': yasifipo_url_for('display_prez', file_= app.yasifipo["refs"][item][lang]['file'], lang=lang, single=True )})
-			elif type_ == "course":
-				items[type_]['items'].append({ 'title': yaml['title'] , 'url': yasifipo_url_for('display_chapter', file_= app.yasifipo["refs"][item][lang]['file'], lang=lang)})
-			elif type_ == "toc":
-				items[type_]['items'].append({ 'title': yaml['title'] , 'url': yasifipo_url_for('display_chapter', file_= app.yasifipo["refs"][item][lang]['file'], up=app.yasifipo["refs"][item][lang]['up'], lang=lang)})
-
-
-	# Sorting items in each type
-	#TODO
-	# for blog --> sorting by date / alphabetically / modif date / sort tag ?
-	# for pages --> sorting alphabetically / modif date / sort tag ?
-	# for course --> sorting alphabetically / modif date of any file in course / sort tag ?
-	# for single prez --> sorting alphabetically / modif date of single prez / sort tag ?
-	# for toc --> sorting alphabetically / order in corresponding course / modif date of any file in course / sort tag ?
+	items = get_category_items(category, lang)
 
 	# retrieve type_ sorting & labels
 	with open(app.config["CONFIG_DIR"] + "types") as fil_:
