e4a56a98a630078d9e2e58a00c2b0dc660df305d
Julien Duroure <julien.duroure@gmail.com>
2017-12-12 11:27:06 +0100

    Adding langs for pages

--- yasifipo/app/modules/site/view/page.py
+++ yasifipo/app/modules/site/view/page.py
@@ -20,7 +20,7 @@ def render_page(file_):
 		page.lang = set_lang(yaml)
 
 		#TODO tags
-		#TODO langs
+		page.langs = get_langs_from_ref(yaml)
 
 		if 'cucumber' not in yaml.keys() or ('cucumber' in yaml.keys() and yaml['cucumber'] != False):
 			page.display['cucumber'] = True
