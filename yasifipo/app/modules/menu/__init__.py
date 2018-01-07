91c20b12c269297ed4034b9af935183fe4eb41d9
Julien Duroure <julien.duroure@gmail.com>
2018-01-07 19:02:08 +0100

    Check if menu is a real menu file

--- yasifipo/app/modules/menu/__init__.py
+++ yasifipo/app/modules/menu/__init__.py
@@ -15,17 +15,18 @@ def init_menu_data():
 			with open(app.config["MENU_DIR"] + "/" + file_, encoding='utf-8') as fil_data:
 				yaml = load(fil_data)
 
-				app.yasifipo["menu"][yaml['slug']] = {}
-				app.yasifipo["menu"][yaml['slug']]['description'] = {}
-				for lang in yaml['description'].keys():
-					app.yasifipo["menu"][yaml['slug']]['description'][lang] = yaml['description'][lang]
-					app.yasifipo["menu"][yaml['slug']]['items'] = []
-					for item in yaml['items']:
-						it_ = {}
-						it_['description'] = {}
-						for lang in item['description'].keys():
-							it_['description'][lang] = item['description'][lang]
-						it_['url'] = {}
-						for lang in item['url'].keys():
-							it_['url'][lang] = item['url'][lang]
-						app.yasifipo["menu"][yaml['slug']]['items'].append(it_)
+				if 'slug' in yaml.keys():
+					app.yasifipo["menu"][yaml['slug']] = {}
+					app.yasifipo["menu"][yaml['slug']]['description'] = {}
+					for lang in yaml['description'].keys():
+						app.yasifipo["menu"][yaml['slug']]['description'][lang] = yaml['description'][lang]
+						app.yasifipo["menu"][yaml['slug']]['items'] = []
+						for item in yaml['items']:
+							it_ = {}
+							it_['description'] = {}
+							for lang in item['description'].keys():
+								it_['description'][lang] = item['description'][lang]
+							it_['url'] = {}
+							for lang in item['url'].keys():
+								it_['url'][lang] = item['url'][lang]
+							app.yasifipo["menu"][yaml['slug']]['items'].append(it_)
