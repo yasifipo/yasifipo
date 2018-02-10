2aaf94016675d49afc432bb9ec63db3787fa397b
Julien Duroure <julien.duroure@gmail.com>
2018-02-10 22:39:43 +0100

    Manage not deleted files/dirs on build directory when re-freezing

--- yasifipo/app/manage.py
+++ yasifipo/app/manage.py
@@ -44,6 +44,10 @@ def freeze(data_path, display_all):
 	if 'freeze_dir' in app.yasifipo['config']:
 		app.config['FREEZER_DESTINATION'] = app.config['DATA_DIR'] + app.yasifipo['config']['freeze_dir']
 
+	# manage not deleted files/dir in build directory, based on data config
+	if 'freeze_destignation_ignore' in app.yasifipo['config']:
+		app.config['FREEZER_DESTINATION_IGNORE'] = app.yasifipo['config']['freeze_destignation_ignore']
+
 	if 'dont_freeze' in app.yasifipo['config'] and app.yasifipo['config']['dont_freeze'] == True:
 		print("Can't freeze. Check config")
 		return
--- yasifipo/app/modules/init/_data_minimal/config/config.md
+++ yasifipo/app/modules/init/_data_minimal/config/config.md
@@ -21,4 +21,5 @@ layout_collection: 'collection/collection.html'
 site_version: 0.0.1
 dont_freeze: False
 freeze_dir: build
+freeze_destignation_ignore: []
 ---
