9c75884f9a0b32344a9a36c06b84f42fcf49fcaa
Julien Duroure <julien.duroure@gmail.com>
2018-10-09 22:24:54 +0200

    Fix bug in template when coming from run.py

--- yasifipo/app/run.py
+++ yasifipo/app/run.py
@@ -4,8 +4,9 @@ from modules.site import *
 from main import *
 
 load_config()
-templates_add_loader(app.config['TEMPLATES_DIR']) #must be before Plugin registration
+templates_add_loader(app.config['APPLICATION_DIR'] + "/" + "templates/", init=True)
 run_data_read(app)
+templates_add_loader(app.config['TEMPLATES_DIR'] + app.yasifipo["config"]["theme"]) #must be before Plugin registration
 
 if 'yasifipo_subdirectory' in app.yasifipo['config'] and app.yasifipo['config']['yasifipo_subdirectory'] != '':
     app.register_blueprint(admin, url_prefix="/" + app.yasifipo['config']['yasifipo_subdirectory'] + "/admin")
