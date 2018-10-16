d4417decc0fa80c028ab7b021a058ba18f7ad784
Julien Duroure <julien.duroure@gmail.com>
2018-10-16 21:50:18 +0200

    Copy css / js / template from app to project data when initializing a new project

--- yasifipo/app/modules/init/__init__.py
+++ yasifipo/app/modules/init/__init__.py
@@ -1,6 +1,21 @@
-from shutil import copytree
+import shutil
+import os
 
+def yasifipo_copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 def init_yasifipo_minimal(directory):
 	print("Creating minimal data directory...")
-	copytree("app/modules/init/_data_minimal", directory)
+	shutil.copytree("app/modules/init/_data_minimal", directory)
+
+	# copy templates
+	yasifipo_copytree("app/templates/default", directory + "/templates/default/")
+
+	# copy css + js
+	yasifipo_copytree("app/static", directory + "/static/")
